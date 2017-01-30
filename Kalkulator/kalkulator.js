4f1d5adf7a234e03167f8f5f2861f87a79865c67
Raxzz <kevinrene@hotmail.no>
2017-01-30 11:41:42 +0100

    Revert "La till gange og minus"
    This reverts commit 647261978564b8212499a5c270b4ff1232b09606.

--- Kalkulator/kalkulator.js
+++ Kalkulator/kalkulator.js
@@ -1,33 +1,6 @@
-/*command for pluss*/
-function regnUt() {
- var textb1 = parseInt(document.getElementById("tall1").value);
- var textb2 = parseInt(document.getElementById("tall2").value);
- document.getElementById("resultat").innerHTML = "svaret er: " + leggsammen(textb1, textb2);
-}
-
-function leggsammen(tall1, tall2) {
- return (tall1+tall2);
-
-}
-/*command for munus*/
-function minus() {
- var textb1 = parseInt(document.getElementById("minus1").value);
- var textb2 = parseInt(document.getElementById("minus2").value);
- document.getElementById("minusresultat").innerHTML = "svaret er: " + trekkfra(textb1, textb2);
-}
-
-function trekkfra(minus1, minus2) {
- return (minus1-minus2);
-
-}
-
-function gange() {
- var textb1 = parseInt(document.getElementById("gange1").value);
- var textb2 = parseInt(document.getElementById("gange2").value);
- document.getElementById("gangeresultat").innerHTML = "svaret er: " + Gangesvar(textb1, textb2);
-}
-
-function gangesvar(gange1, gange2) {
- return (gange1*gange2);
-
+window.onload = function() {
+  var tall1 = prompt("skriv in første tall");
+  var tall2 = prompt("skriv in andre tall");
+  document.getElementById("kalkulator").innerHTML = "svaret er " + (parseInt(tall1) + parseInt(tall2));
+  alert("svaret er " + (parseFloat(tall1)-parseFloat(tall2)));
 }
