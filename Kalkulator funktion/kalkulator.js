fa12d1c22c9a7a30873e2d537dde80e33087f560
Raxzz <kevinrene@hotmail.no>
2017-01-30 12:10:13 +0100

    sletta alt med et uhel

--- Kalkulator funktion/kalkulator.js
+++ Kalkulator funktion/kalkulator.js
@@ -1,3 +1,4 @@
+/*command for pluss*/
 function regnUt() {
  var textb1 = parseInt(document.getElementById("tall1").value);
  var textb2 = parseInt(document.getElementById("tall2").value);
@@ -6,5 +7,27 @@ function regnUt() {
 
 function leggsammen(tall1, tall2) {
  return (tall1+tall2);
+
+}
+/*command for munus*/
+function minus() {
+ var textb1 = parseInt(document.getElementById("minus1").value);
+ var textb2 = parseInt(document.getElementById("minus2").value);
+ document.getElementById("minusresultat").innerHTML = "svaret er: " + trekkfra(textb1, textb2);
+}
+
+function trekkfra(minus1, minus2) {
+ return (minus1-minus2);
+
+}
+
+function gange() {
+ var textb1 = parseInt(document.getElementById("gange1").value);
+ var textb2 = parseInt(document.getElementById("gange2").value);
+ document.getElementById("gangeresultat").innerHTML = "svaret er: " + Gangesvar(textb1, textb2);
+}
+
+function gangesvar(gange1, gange2) {
+ return (gange1+gange2);
 
 }
